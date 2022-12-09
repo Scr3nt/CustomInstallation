e253db2d06c8ca88069a5e62c082cc7a8dcf0551
Gino Dzin <56580186+Scr3nt@users.noreply.github.com>
2022-12-09 15:44:46 +0100

    🚧 husky test

--- app/index.tsx
+++ app/index.tsx
@@ -8,6 +8,7 @@ export default function Home() {
       <TouchableOpacity testID="click-user" onPress={() => link.push("/user")}>
         <Text>Go vers user</Text>
       </TouchableOpacity>
+
       <TouchableOpacity
         testID="click-settings"
         onPress={() => link.push("/settings")}
