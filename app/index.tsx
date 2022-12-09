f78b7e8ba691f53da4a7feb2cda4fb9f95ebe881
Gino Dzin <56580186+Scr3nt@users.noreply.github.com>
2022-12-09 17:32:44 +0100

    :lipstick: add theme

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,28 @@
+import theme from "@/theme";
+import { ThemeProvider } from "@shopify/restyle";
 import { useLink } from "expo-router";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Home() {
   const link = useLink();
   return (
-    <View testID="home" style={styles.container}>
-      <TouchableOpacity testID="click-user" onPress={() => link.push("/user")}>
-        <Text>Go vers user</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        testID="click-settings"
-        onPress={() => link.push("/settings")}
-      >
-        <Text>Go vers settings</Text>
-      </TouchableOpacity>
-      <Text>Tu es sur home</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <View testID="home" style={styles.container}>
+        <TouchableOpacity
+          testID="click-user"
+          onPress={() => link.push("/user")}
+        >
+          <Text>Go vers user</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          testID="click-settings"
+          onPress={() => link.push("/settings")}
+        >
+          <Text>Go vers settings</Text>
+        </TouchableOpacity>
+        <Text>Tu es sur home</Text>
+      </View>
+    </ThemeProvider>
   );
 }
 
---
+++ theme.ts
@@ -0,0 +1,17 @@
+const theme = {
+  colors: {
+    green: "#05E498",
+    yellow: "#FED234",
+    orange: "#FF7C44",
+    red: "#FE3F60",
+    pink: "#E42FE4",
+    purple: "#C04BFA",
+    blue: "#08A8FE",
+    white: "#EAE8E2",
+    grey: "#7D7D7D",
+    black: "#111111",
+  },
+};
+
+export type Theme = typeof theme;
+export default theme;
